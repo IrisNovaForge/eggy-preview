4890753776343f406d69828d1f8d14ab2cb10d80
IrisNovaForge <gameproject2026tj@gmail.com>
2026-08-22 07:57:21 +0900

    publish: expanded joystick stop zone

--- plugins/brick-breaker/debug/boot.js
+++ plugins/brick-breaker/debug/boot.js
@@ -45,7 +45,7 @@ window.createEggMesh=function(bodyColor,accentColor,characterId){
 window._updateCharacterPremiumRig=function(){};
 window._animateCuteCharacterDetails=function(){};
 
-const BUILD='20260822.42';
+const BUILD='20260822.43';
 function loadScript(source){
     return new Promise(function(resolve,reject){
         var script=document.createElement('script');script.src=source+(source.indexOf('?')>=0?'&':'?')+'v='+BUILD;script.onload=resolve;script.onerror=reject;document.body.appendChild(script);
